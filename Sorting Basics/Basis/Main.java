2b863b5a143e0290a155772ce00b46a23eeecc8d
Fox <36572168+Fox520@users.noreply.github.com>
2019-03-20 10:38:16 +0200

    Create Main.java

---
+++ Sorting Basics/Basis/Main.java
@@ -0,0 +1,66 @@
+public class Main{
+    public static void main(String[] args) {
+        String testMethod = "selection";
+        switch(testMethod){
+            case "selection":
+                //testing selection sort
+                int[] unsortedSelection = {5, 3, 1, 2, 4, 10, 0};
+                for (int A : selectionSort(unsortedSelection)) {
+                    System.out.print(A + " ");
+                }
+                break;
+
+            case "insertion":
+                //testing insertion sort
+                int[] unsortedInsertion = {5, 3, 1, 2, 4, 10, 0};
+                for (int A : sortInsert(unsortedInsertion)) {
+                    System.out.print(A + " ");
+                }
+                break;
+        }
+        
+    }
+
+    public static int[] selectionSort(int[] array) {
+        int n = array.length - 1;
+        //no need to sort array with 1 item
+        if (n <= 0) {
+            return array;
+        } else {
+            //i is the furthest left unsorted element
+            for (int i = 0; i < n; i++) {
+                int min = i;
+                //the i+1 & <= is important
+                //as we comparing starting from the elements ahead of i
+                //the <= is to reach the end of the array
+                for (int j = i + 1; j <= n; j++) {
+                    //find min index
+                    if (array[j] > array[min]) {
+                        min = j;
+                    }
+                }
+                int temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+            return array;
+        }
+    }
+
+    //followed pseudocode on wikipedia (https://en.wikipedia.org/wiki/Insertion_sort)
+    public static int[] sortInsert(int[] A) {
+        //0 is trivially sorted
+        int i = 1;
+        while (i < A.length) {
+            int j = i;
+            while (j > 0 && A[j - 1] > A[j]) {
+                int temp = A[j - 1];
+                A[j - 1] = A[j];
+                A[j] = temp;
+                j--;
+            }
+            i++;
+        }
+        return A;
+    }
+}
